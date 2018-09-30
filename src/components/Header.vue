c66a1f3e214110b637a7edd456cd5927e09d56f3
lichao779@sina.com <wb-lc186736@alibaba-inc.com>
2018-09-30 21:53:41 +0800

    update header comp

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="header">
+        <div class="logo">
+        <img src="//static2.cnodejs.org/public/images/cnodejs_light.svg" alt="logo">
+        </div>
+        <ul>
+            <li>
+                <a href="#">首页</a>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+/* eslint-disable */
+export default {
+  name: "Header"
+};
+</script>
+<style scoped>
+.header {
+  background-color: #444;
+  padding: 5px 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.logo {
+  vertical-align: middle;
+}
+img {
+  width: 130px;
+  margin: 0 auto;
+}
+ul {
+  list-style: none;
+  margin: 0;
+}
+ul a {
+  color: #777;
+  text-decoration: none;
+  text-shadow: none;
+  color: #ccc;
+}
+</style>
